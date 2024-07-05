872cb789c6314ecbc96e98be1eb447a10a780ba8
JanKuczma <jan.kuczma@cardinals.cc>
2024-07-05 09:55:17 +0200

    fix updating rates

--- amm/contracts/stable_pool/lib.rs
+++ amm/contracts/stable_pool/lib.rs
@@ -287,7 +287,7 @@ pub mod stable_pool {
             let current_time = self.env().block_timestamp();
             let mut rate_changed = false;
             for rate in self.pool.token_rates.iter_mut() {
-                rate_changed = rate_changed || rate.update_rate(current_time);
+                rate_changed = rate_changed | rate.update_rate(current_time);
             }
             if rate_changed {
                 Self::env().emit_event(RatesUpdated {
@@ -718,7 +718,7 @@ pub mod stable_pool {
             let current_time = self.env().block_timestamp();
             let mut rate_changed = false;
             for rate in self.pool.token_rates.iter_mut() {
-                rate_changed = rate_changed || rate.update_rate_no_cache(current_time);
+                rate_changed = rate_changed | rate.update_rate_no_cache(current_time);
             }
             if rate_changed {
                 Self::env().emit_event(RatesUpdated {
