c13c2b7f3dfcca1ba67fbe8aef38ad2949debb6f
JanKuczma <jan.kuczma@cardinals.cc>
2024-07-08 11:30:38 +0200

    typo

--- amm/contracts/stable_pool/lib.rs
+++ amm/contracts/stable_pool/lib.rs
@@ -308,10 +308,11 @@ pub mod stable_pool {
             self.pool.fee_receiver
         }
 
-        // Scaled rates are rates multiplied by precision. They are assumed to fit in u128.
-        // If TOKEN_TARGET_DECIMALS is 18 and RATE_DECIMALS is 12, then rates not exceeding ~340282366 should fit.
-        // That's because if precision <= 10^18 and rate <= 10^12 * 340282366, then rate * precision < 2^128.
-        // NOTE: Rates should be updated prior ro calling this function
+        /// Scaled rates are rates multiplied by precision. They are assumed to fit in u128.
+        /// If TOKEN_TARGET_DECIMALS is 18 and RATE_DECIMALS is 12, then rates not exceeding ~340282366 should fit.
+        /// That's because if precision <= 10^18 and rate <= 10^12 * 340282366, then rate * precision < 2^128.
+        ///
+        /// NOTE: Rates should be updated prior to calling this function
         fn get_scaled_rates(&self) -> Result<Vec<u128>, MathError> {
             self.pool
                 .token_rates
@@ -354,7 +355,8 @@ pub mod stable_pool {
             Ok((token_in_id, token_out_id))
         }
         /// Calculates lpt equivalent of the protocol fee and mints it to the `fee_to` if one is set.
-        /// NOTE: Rates should be updated prior ro calling this function
+        /// 
+        /// NOTE: Rates should be updated prior to calling this function
         fn mint_protocol_fee(&mut self, fee: u128, token_id: usize) -> Result<(), StablePoolError> {
             if let Some(fee_to) = self.fee_to() {
                 let protocol_fee = self.pool.fees.protocol_trade_fee(fee)?;
